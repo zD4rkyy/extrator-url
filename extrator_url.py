a04bff0c1bfcfa9f7d911ac6b62c32361c0b012c
zD4rkyy <103229814+zD4rkyy@users.noreply.github.com>
2022-05-14 22:08:11 -0300

    Termino do projeto

--- extrator_url.py
+++ extrator_url.py
@@ -47,13 +47,15 @@ class ExtratorURL:
     def __str__(self):
         return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
 
+    def __eq__(self, other):
+        return self.url == other.url
+
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
 extrator_url2 = ExtratorURL(url)
-# print("O tamanho da URL: ", len(extrator_url))
-# print(extrator_url)
+
+print(id(extrator_url))
+print(id(extrator_url2))
 
 print(extrator_url == extrator_url2)
 
-# valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-# print(valor_quantidade)
