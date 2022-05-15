e80a6bac3358f49a4a6d1e876d5026e4f9dec6c1
zD4rkyy <103229814+zD4rkyy@users.noreply.github.com>
2022-05-14 21:57:06 -0300

    pep8

---
+++ main.py
@@ -0,0 +1,28 @@
+# url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+url = " "
+
+# Sanitização da URL
+url = url.strip()
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia!")
+
+# Separa a base e os parametros
+indice_interrogacao = url.find("?")
+url_base = url[0:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+# Busca o valor de um parametro
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find("&", indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
+
